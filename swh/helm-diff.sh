17cf2d9116ffd88275e0ae15287a8cc401db8998
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2023-10-17 16:28:44 +0200

    swh/helm-diff: Fix diff tool

--- swh/helm-diff.sh
+++ swh/helm-diff.sh
@@ -27,42 +27,54 @@ for environment in "staging" "production"; do
   HELM_CMD="helm template test $APP --values values-swh-application-versions.yaml --values $APP/values.yaml --values $APP/values/default.yaml --values $APP/values/$environment/default.yaml --values"
 
   git checkout $MAIN_BRANCH
-  echo "[$APP] Generate config in $MAIN_BRANCH branch for environment ${environment}..."
-  output="$TMPDIR/${environment}.before"
 
-  $HELM_CMD $APP/values/$environment/swh.yaml > $output
-  $HELM_CMD $APP/values/$environment/swh-cassandra.yaml >> $output
+  for namespace in "swh" "swh-cassandra" "swh-cassandra-next-version"; do
+    echo "[$APP] Generate config in $MAIN_BRANCH branch for environment ${environment}, namespace ${namespace}..."
+    output="$TMPDIR/${environment}-${namespace}.before"
 
-  if [ "$environment" = "staging" ]; then
-    $HELM_CMD $APP/values/$environment/swh-cassandra.yaml \
-              --values $APP/values/$environment/overrides/swh-cassandra-next-version.yaml >> $output
-  fi
+    if [ -f $APP/values/$environment/$namespace.yaml ]; then
+      $HELM_CMD $APP/values/$environment/$namespace.yaml > $output
+    elif [ -f $APP/values/$environment/overrides/$namespace.yaml ]; then
+      # Deal with cassandra-next-version
+      $HELM_CMD $APP/values/$environment/swh-cassandra.yaml \
+        --values $APP/values/$environment/overrides/$namespace.yaml > $output
+    fi
+  done
+
 
   # git stash pop
   git checkout $BRANCH
-  echo "[$APP] Generate config in $BRANCH branch for environment ${environment}..."
-  output="$TMPDIR/${environment}.after"
+  for namespace in "swh" "swh-cassandra" "swh-cassandra-next-version"; do
+    echo "[$APP] Generate config in $BRANCH branch for environment ${environment}..."
+    output="$TMPDIR/${environment}-${namespace}.after"
 
-  $HELM_CMD $APP/values/$environment/swh.yaml > $output
-  $HELM_CMD $APP/values/$environment/swh-cassandra.yaml >> $output
-
-  if [ "$environment" = "staging" ]; then
-    $HELM_CMD $APP/values/$environment/swh-cassandra.yaml \
-              --values $APP/values/$environment/overrides/swh-cassandra-next-version.yaml >> $output
-  fi
+    if [ -f $APP/values/$environment/$namespace.yaml ]; then
+      $HELM_CMD $APP/values/$environment/$namespace.yaml > $output
+    elif [ -f $APP/values/$environment/overrides/$namespace.yaml ]; then
+      # Deal with cassandra-next-version
+      $HELM_CMD $APP/values/$environment/swh-cassandra.yaml \
+        --values $APP/values/$environment/overrides/$namespace.yaml > $output
+    fi
+  done
 done
 
 # Actually diff the result for each environment
 
 for environment in "staging" "production"; do
-    output="$TMPDIR/${environment}"
+  for namespace in "swh" "swh-cassandra" "swh-cassandra-next-version"; do
+    output="$TMPDIR/${environment}-${namespace}"
+
+    if [ ! -f "${output}.before" ]; then
+        continue
+    fi
 
     echo
     echo
-    echo "------------- diff for environment $environment -------------"
+    echo "------------- diff for environment ${environment} namespace ${namespace} -------------"
     echo
     set +e
     diff -U${CONTEXT_SIZE} $output.before $output.after
     [[ $? -gt 0 ]] || echo "No differences"
     set -e
+  done
 done
