4abf081a264bf2723dc050e73405ddda676bd2dc
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2024-07-10 16:36:30 +0200

    bin/local-cluster-install-deps.sh: Add helm repo add instructions
    Refs. swh/infra/sysadm-environment#5344

--- bin/local-cluster-install-deps.sh
+++ bin/local-cluster-install-deps.sh
@@ -6,6 +6,18 @@ CLUSTER_CONTEXT=${1-kind-local-cluster}
 
 HELM="helm --kube-context $CLUSTER_CONTEXT"
 
+# Install the helm repo dependencies
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add cnpg https://cloudnative-pg.github.io/charts
+helm repo add strimzi https://strimzi.io/charts/
+helm repo add k8ssandra https://helm.k8ssandra.io/stable
+helm repo add jetstack https://charts.jetstack.io
+helm repo add elastic https://helm.elastic.co
+helm repo add ot-helm https://ot-container-kit.github.io/helm-charts/
+helm repo update
+
+# Now actually installs the various operator dependencies
+
 $HELM upgrade --install ingress-nginx ingress-nginx \
      --repo https://kubernetes.github.io/ingress-nginx \
      --namespace ingress-nginx --create-namespace
