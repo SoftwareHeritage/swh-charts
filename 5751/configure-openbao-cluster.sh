013bf1f22feed016076b56e106ba3ec38394ca77
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2026-01-20 11:43:12 +0100

    wip: Generate shared ca to install openbao
    remains to add the vso

--- 5751/configure-openbao-cluster.sh
+++ 5751/configure-openbao-cluster.sh
@@ -30,6 +30,27 @@ elif [[ "$1" == "--cleanup" ]]; then
 #  $KUBECTL_OPENBAO delete namespace "${NS_OPENBAO}" || true
 fi
 
+if [ ! -d $CA_CERT_DIR ]; then
+    mkdir -p $CA_CERT_DIR
+    # Generate private rsa key
+    openssl genrsa -out $CA_CERT_FILEKEY 4096
+
+    # Ensure no issues occurred
+    [ ! -f $CA_CERT_FILEKEY ] && echo "<$CA_CERT_FILEKEY> must exist!" && exit 1
+
+    # Self-signed shared root certificate in between kind clusters
+    openssl req -x509 -new -nodes -key $CA_CERT_FILEKEY \
+            -sha256 -days 3650 \
+            -subj "/CN=shared-local-clusters-ca" \
+            -out $CA_CERT_FILECRT
+    # Ensure no issues occurred
+    [ ! -f $CA_CERT_FILECRT ] && echo "<$CA_CERT_FILECRT> must exist!" && exit 1
+fi
+
+# Inject shared ca
+${KUBECTL_OPENBAO} create secret tls shared-ca \
+  --cert=$CA_CERT_FILECRT --key=$CA_CERT_FILEKEY
+
 ${HELM_OPENBAO} repo add jetstack https://charts.jetstack.io
 ${HELM_OPENBAO} repo add metallb https://metallb.github.io/metallb
 ${HELM_OPENBAO} repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
@@ -70,20 +91,84 @@ spec:
   - "local-metallb-pool-ingress"
 EOF
 
+OPENBAO_VALUES_FILE=$TEMP_DIR/openbao-values.yaml
+cat > "${OPENBAO_VALUES_FILE}" << EOF
+injector:
+  logLevel: trace
+server:
+  logLevel: trace
+  # Add some extra dns records so we don't need an extra dns
+  hostAliases:
+  # Make vso ingress hostname resolvable
+  - ip: ${VSO_INGRESS_IP}
+    hostnames:
+    - ${VSO_INGRESS_HOSTNAME}
+  # Make openbao ingress hostname resolvable in-cluster too
+  - ip: ${OPENBAO_INGRESS_IP}
+    hostnames:
+    - ${OPENBAO_INGRESS_HOSTNAME}
+  # Enable the dev mode (in-memory)
+  dev:
+    enabled: true
+  # Enable ingress so openbao is reachable from outside the cluster too
+  ingress:
+    enabled: true
+    ingressClassName: nginx
+    hosts:
+    - host: ${OPENBAO_INGRESS_HOSTNAME}
+    tls:
+    - secretName: openbao-tls
+      hosts:
+      - ${OPENBAO_INGRESS_HOSTNAME}
+  volumes:
+  - name: ca
+    configMap:
+      name: shared-ca
+  volumeMounts:
+  - name: ca
+    mountPath: /etc/openbao/ca
+    readOnly: true
+  certs:
+    secretName: openbao-tls
+    caBundle: /etc/openbao/ca/ca.crt
+EOF
+
 ${HELM_OPENBAO} upgrade \
   --install openbao openbao/openbao \
-  --set "injector.logLevel=trace" \
-  --set "server.logLevel=trace" \
-  --set "server.dev.enabled=true" \
-  --set "server.ingress.enabled=true" \
-  --set "server.ingress.ingressClassName=nginx" \
-  --set "server.ingress.hosts[0].host=${OPENBAO_INGRESS_HOSTNAME}" \
-  --set "server.hostAliases[0].ip=${VSO_INGRESS_IP}" \
-  --set "server.hostAliases[0].hostnames[0]=${VSO_INGRESS_HOSTNAME}" \
-  -n "${NS_OPENBAO}" \
+  --values "${OPENBAO_VALUES_FILE}" \
+  --namespace "${NS_OPENBAO}" \
   --create-namespace
 
 ${KUBECTL_OPENBAO} wait pod --all --for=condition=Ready --timeout=60s -n "${NS_OPENBAO}"
+
+# Create a cluster issuer shared-ca-issuer and generate a certificate for openbao
+${KUBECTL_OPENBAO} apply -f - <<EOF
+---
+ApiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+  name: shared-ca-issuer
+spec:
+  ca:
+    secretName: shared-ca
+---
+apiVersion: cert-manager.io/v1
+kind: Certificate
+metadata:
+  name: openbao-tls
+  namespace: openbao   # Helm release namespace
+spec:
+  secretName: openbao-tls   # will contain tls.crt & tls.key
+  dnsNames:
+  - ${OPENBAO_INGRESS_HOSTNAME}   # FQDN that the vso pod will use
+  issuerRef:
+    name: shared-ca-issuer
+    kind: ClusterIssuer
+EOF
+
+${KUBECTL_OPENBAO} create configmap \
+  --namespace openbao shared-ca \
+  --from-file=ca.crt="${CA_CERT_FILECRT}"
 
 cat <<EOF
 ##############################################
