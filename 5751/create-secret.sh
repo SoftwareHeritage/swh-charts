3a440a2e2abc132b99d84fd15a983ea274dd4477
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2026-01-28 18:37:30 +0100

    Ensure vso access is unauthorized access when configured accordingly
    Refs. swh/infra/sysadm-environment#5751

--- 5751/create-secret.sh
+++ 5751/create-secret.sh
@@ -28,7 +28,8 @@ shift
 # Display help message
 usage() {
   echo "Usage: $0 CLUSTER_NAME [OPTIONS]"
-  echo -e "\nCreates an openbao secret and configure secret synchronization\n"
+  echo -e "\nConfigure secret synchronization, creates an openbao secret and checks "
+  echo -e "\nthat new secret is stored in the kubernetes cluster\n"
   echo "Options:"
   echo "  --debug     Enable verbose instructions"
   echo "  -h, --help  Display this help message"
@@ -55,8 +56,20 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-STATIC_SECRET_FILE="${TEMP_DIR}/${STATIC_SECRET_FILENAME}"
-cat > "${STATIC_SECRET_FILE}" << EOF
+echo "### Synchronization with authorized mount path should be ok ###"
+
+echo "Write a secret in openbao in authorized (in openbao policy) mount path <$MOUNT>."
+echo "This secret should be synchronized correctly in cluster <$CLUSTER_NAME>."
+
+$KUBECTL_ADMIN wait pod --all --for=condition=Ready --timeout=60s -n "${NS_OPENBAO}"
+POD_NAME=$($KUBECTL_ADMIN get pods -n "${NS_OPENBAO}" -l app.kubernetes.io/name=openbao -o jsonpath="{.items[0].metadata.name}")
+
+$KUBECTL_ADMIN exec "${POD_NAME}" -n "${NS_OPENBAO}" -- /bin/sh -c \
+  "${POD_VAULT_CMD} kv put -mount=${MOUNT} ${OPENBAO_SECRET_PATH} username='demo-user' password='demo-pass'"
+
+echo "1. First, create a VaultStaticSecret to define the secret to synchronize in vso."
+
+$KUBECTL apply -f - <<EOF
 ---
 apiVersion: secrets.hashicorp.com/v1beta1
 kind: VaultStaticSecret
@@ -73,22 +86,53 @@ spec:
     create: true
     name: ${K8S_SECRET_NAME}
 EOF
-${KUBECTL} apply -f "${STATIC_SECRET_FILE}"
 
-# Let's create a secret in the vso cluster
+echo "2. Check the secret is synchronized correctly in the cluster <$CLUSTER_NAME>."
 
-${KUBECTL_ADMIN} wait pod --all --for=condition=Ready --timeout=60s -n "${NS_OPENBAO}"
-POD_NAME=$($KUBECTL_ADMIN get pods -n "${NS_OPENBAO}" \
-  -l app.kubernetes.io/name=openbao -o jsonpath="{.items[0].metadata.name}")
+echo "Waiting for the synchronization to be ok"
+timeout 20s bash -c "until $KUBECTL get secret $K8S_SECRET_NAME --namespace=$NS_APP &>/dev/null; do printf \".\"; sleep 0.2; done"
 
-# Write a secret password
-$KUBECTL_ADMIN exec "${POD_NAME}" -n "${NS_OPENBAO}" -- /bin/sh -c \
-  "${POD_VAULT_CMD} kv put -mount=${MOUNT} ${OPENBAO_SECRET_PATH} username='demo-user' password='demo-pass'"
+[ $? -ne 0 ] && echo "ohoh! We did not find the secret, check your setup!" && exit 1
 
-timeout 60s bash -c \
-  "until ${KUBECTL} get secret ${K8S_SECRET_NAME} --namespace=${NS_APP} &>/dev/null; do echo 'Waiting...'; sleep 1; done"
+SECRET=$(${KUBECTL} get secret ${K8S_SECRET_NAME} --namespace=${NS_APP})
+echo "Secret: $SECRET"
 
-([ $? -ne 0 ] && echo "ohoh! We did not find the secret, check your setup!" && exit 1
+echo "### Synchronization with authorized mount path IS ok! ###"
+echo
 
- SECRET=$(${KUBECTL} get secret ${K8S_SECRET_NAME} --namespace=${NS_APP})
- echo "Secret: $SECRET"
+echo "### Synchronization with unauthorized mount path should be unauthorized access."
+
+echo "1. First, create a VaultStaticSecret to define the secret to synchronize."
+
+$KUBECTL apply -f - <<EOF
+---
+apiVersion: secrets.hashicorp.com/v1beta1
+kind: VaultStaticSecret
+metadata:
+  name: ${UNAUTHORIZED_STATIC_SECRET_NAME}
+  namespace: ${NS_APP}
+spec:
+  vaultAuthRef: ${VAULT_AUTH_NAME}
+  mount: ${UNAUTHORIZED_MOUNT}
+  type: kv-v2
+  path: ${OPENBAO_SECRET_PATH}
+  refreshAfter: 10s
+  destination:
+    create: true
+    name: ${UNAUTHORIZED_K8S_SECRET_NAME}
+EOF
+
+echo "2. Try to synchronize that secret from unauthorized mount point"
+( $KUBECTL get secret $UNAUTHORIZED_K8S_SECRET_NAME --namespace=$NS_APP 2>&1 | \
+  grep -iq "not found" && \
+  echo "Expectedly the secret <$UNAUTHORIZED_K8S_SECRET_NAME> does not exist.") || \
+  ( echo "uhoh! The secret <$UNAUTHORIZED_K8S_SECRET_NAME> exist, it should not." && \
+    exit 2 )
+
+( $KUBECTL describe VaultStaticSecret -n $NS_APP | \
+  grep -iq "permission denied\|403" && \
+  echo "Expectedly the VaultStaticSecret <${UNAUTHORIZED_STATIC_SECRET_NAME}> is denied access by openbao." ) || \
+  ( echo "uhoh! The VaultStaticSecret <${UNAUTHORIZED_STATIC_SECRET_NAME}> got granted access!" && \
+    exit 2 )
+
+echo "### Synchronization with unauthorized mount path IS indeed unauthorized access! ###"
